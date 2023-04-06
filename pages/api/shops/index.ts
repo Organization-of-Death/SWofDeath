1ea87623024346a4556fe1dc24123d67eef5732d
noWarm <jomwit22@gmail.com>
2023-04-07 00:29:56 +0700

    feat: implemented pagination for lists of shops

--- pages/api/shops/index.ts
+++ pages/api/shops/index.ts
@@ -11,7 +11,14 @@ type MassageShopData = {
 async function handler(req: NextApiRequest, res: NextApiResponse<MassageShopData>) {
 
     if (req.method === 'GET') {
-        const massageShops = await prisma.massageShop.findMany();
+        const page_no = parseInt(typeof req.query.page_no === "string" ? req.query.page_no : "0");
+        const page_size = parseInt(typeof req.query.page_size === "string" ? req.query.page_size : "10");
+
+        const skip = page_no * page_size;
+        const massageShops = await prisma.massageShop.findMany({
+            skip,
+            take: page_size,
+        });
         return res.status(200).json({ message: "successfully retrieved massage shops", total: massageShops.length, data: massageShops });
     } else {
         return res.status(405).json({ message: "method not supported" });
