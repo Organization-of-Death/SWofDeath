2591ef4175c60dab68a78b12a56e54558c3ce3e7
Theresa <thutgreybear@gmail.com>
2023-04-10 18:21:32 +0700

    add:generate random url for youtube video

---
+++ pages/api/reservations/random.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Reservation, Role } from "@prisma/client";
+import { prisma } from "@/prisma/utils";
+import { reservationInput, reservationSchema } from "@/pages/types/schema";
+import { sendDiscordMessage } from "@/pages/types/discord";
+import randomstring from "randomstring";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const randomString = randomstring.generate({
+      length: 11,
+      charset: "alphanumeric",
+    });
+
+    // Return the URL to the randomly generated YouTube video
+    res.status(200).json(`https://www.youtube.com/watch?v=${randomString}`);
+  }
+}
+
+export default handler;
